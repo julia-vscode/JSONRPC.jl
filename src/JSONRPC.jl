7e2b6a0379efe2ca36226239754a400f719e5132
David Anthoff <anthoff@berkeley.edu>
2020-05-15 14:13:01 +0200

    Initial version

---
+++ src/JSONRPC.jl
@@ -0,0 +1,9 @@
+module JSONRPC
+
+import JSON, UUIDs
+
+export JSONRPCEndpoint, send_notification, send_request, send_success_response, send_error_response
+
+include("core.jl")
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,5 @@
+using Test
+using JSONRPC
+
+@testset "JSONRPC" begin
+end
