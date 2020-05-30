3feacbb1608bc496816abce9e6e4d79cc8570ece
Shuhei Kadowaki <aviatesk@gmail.com>
2020-05-30 16:29:29 +0900

    add very simple tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,14 @@
 using Test
 using JSONRPC
+using JSONRPC: typed_res
 
 @testset "JSONRPC" begin
+    @testset "check response type" begin
+        @test typed_res(nothing, Nothing) isa Nothing
+        @test typed_res([1,"2",3], Vector{Any}) isa Vector{Any}
+        @test typed_res([1,2,3], Vector{Int}) isa Vector{Int}
+        @test typed_res([1,2,3], Vector{Float64}) isa Vector{Float64}
+        @test typed_res(['f','o','o'], String) isa String
+        @test typed_res("foo", String) isa String
+    end
 end
