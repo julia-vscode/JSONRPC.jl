ac2f49bf7808001a9ee688ddd42a09888730b03a
Sebastian Pfitzner <pfitzseb@gmail.com>
2026-03-09 18:29:43 +0100

    chore: add precompile statements

--- src/packagedef.jl
+++ src/packagedef.jl
@@ -4,3 +4,19 @@ include("pipenames.jl")
 include("core.jl")
 include("typed.jl")
 include("interface_def.jl")
+
+function _precompile_()
+    ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
+
+    E = JSONRPCEndpoint{Base.PipeEndpoint, Base.PipeEndpoint, JSON.Serializations.StandardSerialization}
+    precompile(Base.run, (E,))
+    precompile(send_notification, (E, String, Any))
+    precompile(send_request, (E, String, Any))
+    precompile(send_success_response, (E, Request, Any))
+    precompile(send_error_response, (E, Request, Any, Any, Any))
+    precompile(dispatch_msg, (E, MsgDispatcher, Request))
+    precompile(Base.setindex!, (MsgDispatcher, Function, Any))
+    precompile(get_next_message, (E,))
+end
+
+_precompile_()
