6b1485d29cf2a1c2127251a5552ab1bc93cb1fbd
fromelicks <108532072+fromelicks@users.noreply.github.com>
2026-08-31 10:22:50 +0200

    Refactor JSON serialization adapter

--- src/jsoncompat.jl
+++ src/jsoncompat.jl
@@ -1,13 +1,17 @@
 @static if isdefined(JSON, :JSONStyle)
     const JSONSerialization = JSON.JSONStyle
     const DefaultJSONSerialization = JSON.JSONWriteStyle
-
-    _serialize_json(serialization::JSONSerialization, value) = JSON.json(value; style=serialization)
 else
     const JSONSerialization = JSON.Serialization
     const DefaultJSONSerialization = JSON.StandardSerialization
+end
 
-    _serialize_json(serialization::JSONSerialization, value) = sprint(JSON.show_json, serialization, value)
+function _serialize_json(serialization::JSONSerialization, value)
+    @static if isdefined(JSON, :JSONStyle)
+        return JSON.json(value; style=serialization)
+    else
+        return sprint(JSON.show_json, serialization, value)
+    end
 end
 
 _parse_json(value) = JSON.parse(value; dicttype=Dict{String,Any})
